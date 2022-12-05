06f70f43a2e5023f636ab48df0c5a9d14d59d169
Nathalia Pinheiro <nathaliapinheiro3@outlook.com>
2022-12-05 15:54:02 -0300

    add Angular Materials

--- Conversor/src/app/app.module.ts
+++ Conversor/src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { NgModule } from '@angular/core';
+import { NgModule } from "@angular/core";
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConversorTemperaturaComponent } from './conversor-temperatura/conversor-temperatura.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from "./angular-material.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConversorTemperaturaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    AngularMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Conversor/src/app/conversor-temperatura/conversor-temperatura.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-conversor-temperatura',
+  templateUrl: './conversor-temperatura.component.html',
+  styleUrls: ['./conversor-temperatura.component.css']
+})
+export class ConversorTemperaturaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
