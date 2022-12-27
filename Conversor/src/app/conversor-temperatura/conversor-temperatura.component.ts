0cc5b07459601a7b2ce26034d1f72e0d6fa6b47e
MatheusPereira00 <matheuspereira@ibm.com>
2022-12-27 11:25:35 -0300

    att

--- Conversor/src/app/conversor-temperatura/conversor-temperatura.component.ts
+++ Conversor/src/app/conversor-temperatura/conversor-temperatura.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConversorTemperaturaComponent implements OnInit {
 
+
+  title: string = 'Conversor'
   constructor() { }
 
   ngOnInit(): void {
